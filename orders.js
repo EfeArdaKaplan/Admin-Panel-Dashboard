423409da692725e35a5d8e3a23e36a60de62ddec
Efe Arda KAPLAN <160834814+EfeArdaKaplan@users.noreply.github.com>
2026-01-20 18:44:21 +0300

    Add files via upload

---
+++ orders.js
@@ -0,0 +1,44 @@
+const Orders = [
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber: '85631',
+        paymentStatus: 'Due',
+        shipping: 'Pending',
+    },
+    {
+        productName: 'LARVENDER KF102 Drone',
+        productNumber: '36123',
+        paymentStatus: 'Refunded',
+        shipping: 'Declined',
+    },
+    {
+        productName: 'Ruko F11 Pro Drone',
+        productNumber: '12345',
+        paymentStatus: 'Due',
+        shipping: 'Pending',
+    },
+    {
+        productName: 'Drone with Camera Drone',
+        productNumber: '43242',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered',
+    },
+    {
+        productName: 'GPS 4K Drone',
+        productNumber: '98723',
+        paymentStatus: 'Refunded',
+        shipping: 'Declined',
+    },
+    {
+        productName: 'DJI Air 2S',
+        productNumber: '12457',
+        paymentStatus: 'Due',
+        shipping: 'Pending',
+    },
+    {
+        productName: 'Lozenge Drone',
+        productNumber: '95467',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered',
+    }
+]
